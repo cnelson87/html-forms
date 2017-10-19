2b890f7a181ff733c6d06bf17781563f5541c83b
Chris Nelson <chris.nelson@wearepop.com>
2017-10-18 17:41:38 -0700

    updates

--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -3,9 +3,15 @@ CNJS.Application = {
 
 	init: function () {
 		var self = this;
-		//console.log('CNJS.Application.init');
-
+		// console.log('CNJS.Application.init');
 		$.event.trigger('CNJS:Application:initialized');
+
+		var $form = $('#TestForm');
+
+		var validator = $form.validate({
+			errorContainer: $('#error-message')
+		});
+
 
 	}
 
